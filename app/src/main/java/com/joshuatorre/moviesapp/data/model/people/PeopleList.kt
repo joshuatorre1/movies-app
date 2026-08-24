4c004b0e2f4cf02cbd3aaa43c62fe5d207b905e7
Joshua Torre <joshuatorre.careers@gmail.com>
2026-08-24 19:55:14 +0800

    Create data classes from API

---
+++ app/src/main/java/com/joshuatorre/moviesapp/data/model/people/PeopleList.kt
@@ -0,0 +1,15 @@
+package com.joshuatorre.moviesapp.data.model.people
+
+
+import com.google.gson.annotations.SerializedName
+
+data class PeopleList(
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("results")
+    val people: List<People>,
+    @SerializedName("total_pages")
+    val totalPages: Int,
+    @SerializedName("total_results")
+    val totalResults: Int
+)
